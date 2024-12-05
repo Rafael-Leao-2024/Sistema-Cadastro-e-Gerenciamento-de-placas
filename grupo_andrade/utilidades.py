808c3eaa52f927b4410c05d580fb0f8839b0e8e5
RafaelLeao <146947546+Rafael-Leao-2024@users.noreply.github.com>
2024-12-05 20:42:23 -0300

    editar

---
+++ grupo_andrade/utilidades.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+import pytz
+
+def format_datetime(dt):
+    """Converte a data para o horário local e formata para exibição."""
+    local_tz = pytz.timezone('America/Recife')  # Substitua pelo seu fuso horário
+    utc_dt = dt.replace(tzinfo=pytz.utc)  # Assume que a data está em UTC
+    local_dt = utc_dt.astimezone(local_tz)  # Converte para o horário local
+    return local_dt.strftime('%d/%m/%Y %H:%M')  # Formato desejado
